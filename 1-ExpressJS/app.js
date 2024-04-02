0c0af437a736d06dedb5a589e9c4737d4b6341ea
mdakhlaqueali <mdakhlaqueali@gmail.com>
2024-04-03 02:07:29 +0530

    Routers and Filters

--- 1-ExpressJS/app.js
+++ 1-ExpressJS/app.js
@@ -8,8 +8,11 @@ const shopRoutes = require('./routes/shop.js')
 
 app.use(bodyParser.urlencoded());
 
-app.use(adminRoutes);
-app.use(shopRoutes);
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
 
+app.use((req, res, next)=>{
+    res.status(404).send('<h1>Page not found</h1>')
+})
 
 app.listen(3000);
--- 1-ExpressJS/routes/admin.js
+++ 1-ExpressJS/routes/admin.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-product',(req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="Add Product"><input type="number" name="size" placeholder="Enter size"><button type="submit">Add Product</button></form>');
+    res.send('<form action="/admin/add-product" method="POST"><input type="text" name="title" placeholder="Add Product"><input type="number" name="size" placeholder="Enter size"><button type="submit">Add Product</button></form>');
 });
 
-router.post('/product', (req, res, next)=>{
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next)=>{
     console.log(req.body)
-    res.redirect('/')
+    res.redirect('/shop')
 })
 
 module.exports = router;
